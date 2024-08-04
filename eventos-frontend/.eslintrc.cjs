c6293e87797bbdf30927b915317c21472f02263a
ofekot <ofekot@gmail.com>
2024-08-04 15:52:46 +0300

    init

---
+++ eventos-frontend/.eslintrc.cjs
@@ -0,0 +1,22 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2021: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+    "plugin:prettier/recommended",
+    'prettier'
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['react-refresh', 'unused-imports'],
+  rules: {
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
+    "react/react-in-jsx-scope": "off",
+    "unused-imports/no-unused-imports": "error"
+  },
+}
