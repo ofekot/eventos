8393a1a46f50247aac537b675be71eda2b133668
ofekot <ofekot@gmail.com>
2024-08-04 20:36:27 +0300

    add theme

--- eventos-frontend/src/components/NavBar/index.tsx
+++ eventos-frontend/src/components/NavBar/index.tsx
@@ -1,12 +1,21 @@
 import { AppBar, Toolbar, Typography } from '@mui/material';
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import { UrlRoute } from '../../routes';
 
 const NavBar: React.FC = () => {
+  const navigate = useNavigate();
   return (
     <>
       <AppBar position="fixed">
         <Toolbar>
-          <Typography variant="h6" noWrap>
+          <Typography
+            variant="h6"
+            noWrap
+            color="#f55742"
+            fontWeight="bold"
+            sx={{ cursor: 'pointer' }}
+            onClick={() => navigate(UrlRoute.Home)}>
             Eventos
           </Typography>
         </Toolbar>
--- eventos-frontend/src/main.tsx
+++ eventos-frontend/src/main.tsx
@@ -5,14 +5,17 @@ import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 import { ReactRouter6Adapter } from 'use-query-params/adapters/react-router-6';
 import { QueryParamProvider } from 'use-query-params';
-import { CssBaseline } from '@mui/material';
+import { CssBaseline, ThemeProvider } from '@mui/material';
+import { theme } from './theme.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <CssBaseline />
     <BrowserRouter>
       <QueryParamProvider adapter={ReactRouter6Adapter}>
-        <App />
+        <ThemeProvider theme={theme}>
+          <App />
+        </ThemeProvider>
       </QueryParamProvider>
     </BrowserRouter>
   </React.StrictMode>
